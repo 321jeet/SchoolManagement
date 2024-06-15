73b24240f37cc4bf1a318f45e2cf7bd474333662
Sarvajeet Chauhan <91889040+Jeet7965@users.noreply.github.com>
2024-06-15 18:40:19 +0530

    Update settings.py

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'school_database',
         'USER':'root',
-        'PASSWORD':'Kish@n65',
+        'PASSWORD':'*******',
         'HOST':'localhost',
         'PORT':'3306'
 
